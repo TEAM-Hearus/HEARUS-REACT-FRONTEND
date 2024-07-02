d2d21ad3a5f850250be1a1a8e53fab28fa033003
김나연 <apinksky00@gmail.com>
2024-07-02 14:38:26 +0900

    feat: 랜딩페이지 무료로 시작하기 버튼 UI

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,5 +1,11 @@
+import styles from './Landing.module.scss';
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <main className={styles.firstBg}>
+      <button className={styles.loginBtn}>무료로 시작하기</button>
+    </main>
+  );
 };
 
 export default Landing;
