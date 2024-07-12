be1f2f7b09ab000925934bc896ba018a2295a77a
김나연 <apinksky00@gmail.com>
2024-07-12 20:49:00 +0900

    feat: 헤더 타이머 기능 추가

--- src/components/headers/RecordHeader/RecordHeader.tsx
+++ src/components/headers/RecordHeader/RecordHeader.tsx
@@ -1,10 +1,38 @@
 import styles from './Recordheader.module.scss';
 import Back from '../../../assets/images/arrow/back.svg?react';
 import { Link } from 'react-router-dom';
+import { useEffect, useRef, useState } from 'react';
+import { formatTime } from '../../../utils/record';
 
 const RECORD_TITLE = '새로운 녹음-240711';
 
 const RecordHeader = () => {
+  const [seconds, setSeconds] = useState(0);
+  const intervalRef = useRef<number | null>(null);
+
+  useEffect(() => {
+    startTimer();
+    return () => stopTimer();
+  }, []);
+
+  const startTimer = () => {
+    if (intervalRef.current !== null) return;
+    intervalRef.current = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+  };
+
+  const stopTimer = () => {
+    if (intervalRef.current === null) return;
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+  };
+
+  const handleOnClickQuitBtn = () => {
+    stopTimer();
+    setSeconds(0);
+  };
+
   return (
     <header className={styles.container}>
       <Link to="/home">
@@ -15,8 +43,10 @@ const RecordHeader = () => {
         <button className={styles.tagBtn}>태그</button>
       </div>
       <div className={styles.timerContainer}>
-        <p className={styles.timer}>{'00:00:00'}</p>
-        <button className={styles.quitBtn}>종료</button>
+        <p className={styles.timer}>{formatTime(seconds)}</p>
+        <button className={styles.quitBtn} onClick={handleOnClickQuitBtn}>
+          종료
+        </button>
       </div>
     </header>
   );
