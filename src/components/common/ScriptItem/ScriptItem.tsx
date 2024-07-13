adf8fa90f1a84a3e8b56e88098f4976414d3d3d1
Bella <kbee1202@gmail.com>
2024-07-13 10:23:23 +0900

    feat: 스크립트 카드 컴포넌트 추가

---
+++ src/components/common/ScriptItem/ScriptItem.tsx
@@ -0,0 +1,18 @@
+import styles from './ScriptItem.module.scss';
+
+interface IScriptProps {
+  id: number;
+  title: string;
+  content: string;
+  date: string;
+}
+const ScriptItem = ({ id, title, content, date }: IScriptProps) => {
+  return (
+    <div className={styles.postItContainer}>
+      <div className={styles.title}>{title}</div>
+      <div className={styles.content}>{content}</div>
+      <span className={styles.date}>{date}</span>
+    </div>
+  );
+};
+export default ScriptItem;
