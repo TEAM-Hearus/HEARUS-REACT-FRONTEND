db5f5786aa2a6ee110df1702f87f5c2a6f091bbe
김나연 <apinksky00@gmail.com>
2024-07-27 17:13:46 +0900

    design: 메인페이지 첫 화면

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,24 +1,28 @@
-import { useState } from 'react';
-import SignupModal from '../../components/modals/SignupModal/SignupModal';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/logo/landing-logo.svg?react';
 import styles from './Landing.module.scss';
 
 const Landing = () => {
-  const [isLoginClicked, setIsLoginClicked] = useState(false);
-  const openModal = () => {
-    setIsLoginClicked(true);
-  };
-  const closeModal = () => {
-    setIsLoginClicked(false);
-  };
   return (
-    <>
-      <main className={styles.firstBg}>
-        <button className={styles.loginBtn} onClick={openModal}>
-          무료로 시작하기
-        </button>
-      </main>
-      {isLoginClicked && <SignupModal closeModal={closeModal} />}
-    </>
+    <main>
+      <section className={styles.topBgContainer}>
+        <header className={styles.header}>
+          <Link to="/login" className={styles.loginBtn}>
+            로그인
+          </Link>
+          <Link to="/login" className={styles.startBtn}>
+            무료로 시작하기
+          </Link>
+        </header>
+        <article className={styles.titleContainer}>
+          <div className={styles.mainTitle}>
+            <Logo />
+            <h1 className={styles.h1}>HEARUS</h1>
+          </div>
+          <p className={styles.subTitle}>SIOGANNN SIOGANNN</p>
+        </article>
+      </section>
+    </main>
   );
 };
 
