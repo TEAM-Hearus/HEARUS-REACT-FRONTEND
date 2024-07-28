71312db29d4d9104593cb80561fc59b28835a610
김나연 <apinksky00@gmail.com>
2024-07-28 19:26:30 +0900

    feat: 로그인 로딩중 gif 추가

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -5,43 +5,47 @@ import styles from './Login.module.scss';
 
 const Login = () => {
   const handleGoogleLoginClick = () => {
-    window.location.href = `${API_URL}/oauth2/authorization/google`;
+    const url = `${API_URL}/oauth2/authorization/google`;
+    console.log('Redirecting to:', url);
+    window.location.href = url;
   };
   return (
     <div className={styles.bg}>
-      <form className={styles.loginForm}>
-        <h1 className={styles.h1}>로그인</h1>
-        <div className={styles.inputBox}>
-          <label className={styles.label}>
-            이메일
-            <input
-              type="email"
-              placeholder="이메일을 입력하세요"
-              className={styles.input}
-            />
-          </label>
-        </div>
-        <div className={styles.inputBox}>
-          <label className={styles.label}>
-            비밀번호
-            <input
-              type="password"
-              placeholder="비밀번호를 입력하세요"
-              className={styles.input}
-            />
-          </label>
-          <span className={styles.checkboxContainer}>
-            <input
-              type="checkbox"
-              id="remember"
-              className={styles.checkBoxInput}
-            />
-            <label className={styles.checkBoxLabel} htmlFor="remember">
-              로그인 유지하기
+      <div className={styles.loginContainer}>
+        <form className={styles.loginForm}>
+          <h1 className={styles.h1}>로그인</h1>
+          <div className={styles.inputBox}>
+            <label className={styles.label}>
+              이메일
+              <input
+                type="email"
+                placeholder="이메일을 입력하세요"
+                className={styles.input}
+              />
             </label>
-          </span>
-        </div>
-        <button className={styles.loginBtn}>로그인</button>
+          </div>
+          <div className={styles.inputBox}>
+            <label className={styles.label}>
+              비밀번호
+              <input
+                type="password"
+                placeholder="비밀번호를 입력하세요"
+                className={styles.input}
+              />
+            </label>
+            <span className={styles.checkboxContainer}>
+              <input
+                type="checkbox"
+                id="remember"
+                className={styles.checkBoxInput}
+              />
+              <label className={styles.checkBoxLabel} htmlFor="remember">
+                로그인 유지하기
+              </label>
+            </span>
+          </div>
+          <button className={styles.loginBtn}>로그인</button>
+        </form>
         <span className={styles.dividingLine}>
           <hr className={styles.line} />
           <p>계정 연동하기</p>
@@ -55,7 +59,7 @@ const Login = () => {
             <img src={Kakao} alt="Kakao Logo" />
           </button>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
