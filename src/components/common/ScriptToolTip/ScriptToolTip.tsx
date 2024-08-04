5bf3d1164975381a561ff9471cbc0114be27070b
김나연 <apinksky00@gmail.com>
2024-08-04 14:20:57 +0900

    feat: 스크립트 누르면 스크립트 상세 모달 띄우기

--- src/components/common/ScriptToolTip/ScriptToolTip.tsx
+++ src/components/common/ScriptToolTip/ScriptToolTip.tsx
@@ -2,25 +2,58 @@ import { useQuery } from '@tanstack/react-query';
 import { getLectureByScheduleElement } from '../../../apis/schedule';
 import ScriptIcon from '../../../assets/images/nav/my-script-inactive.svg?react';
 import styles from './ScriptToolTip.module.scss';
+import ScriptDetailModal from '../../modals/ScriptDetailModal/ScriptDetailModal';
+import { useEffect, useState } from 'react';
 
 interface IProps {
   id: number;
 }
 
 const ScriptToolTip = ({ id }: IProps) => {
+  const [selectedScriptId, setSelectedScriptId] = useState<string | null>(null);
+
   const { data } = useQuery({
     queryKey: ['tooltip', id],
     queryFn: () => getLectureByScheduleElement(id),
   });
 
+  const handleClick = (scriptId: string) => {
+    setSelectedScriptId(scriptId);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedScriptId(null);
+  };
+
+  useEffect(() => {
+    if (selectedScriptId !== null) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [selectedScriptId]);
+
   return (
     <div className={styles.container}>
       {data?.map((lecture) => (
-        <div className={styles.tooltipItem}>
+        <div
+          className={styles.tooltipItem}
+          onClick={() => handleClick(lecture.id)}
+        >
           {lecture.name}
           <ScriptIcon />
         </div>
       ))}
+      {selectedScriptId !== null && (
+        <ScriptDetailModal
+          scriptId={selectedScriptId}
+          closeModal={handleCloseModal}
+        />
+      )}
     </div>
   );
 };
