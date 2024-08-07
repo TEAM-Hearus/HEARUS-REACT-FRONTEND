4835058ac03c6dfa326ee0ab1f232911a326f91e
김나연 <apinksky00@gmail.com>
2024-08-07 22:37:13 +0900

    fix: 회원가입 로그인 폼 폰트 적용

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,3 @@
 export const API_URL: string = import.meta.env.VITE_API_URL;
 
-export const token = //임시 token
-  'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIwMTZiZDAyOC03OGMxLTRjZTUtYjlhNC1hZDc0Y2ZlYzNkZTIiLCJyb2xlIjoiVVNFUiIsImV4cCI6MTcyMzAyNjg0NH0.3M5sOlFIxFE8KI44gTcuQhchB8pVZy15fE_CRRiH9ak';
-
-// export const token = localStorage.getItem('token');
+export const token = localStorage.getItem('token');
