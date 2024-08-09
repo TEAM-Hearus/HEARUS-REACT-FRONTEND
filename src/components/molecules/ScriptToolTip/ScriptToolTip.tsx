825045bb88a0f6bfb8560f780b906b3909e1f26c
김나연 <apinksky00@gmail.com>
2024-08-09 20:09:17 +0900

    fix: 툴팁이 시간표 아이템에 가려지는것 방지

--- src/components/molecules/ScriptToolTip/ScriptToolTip.tsx
+++ src/components/molecules/ScriptToolTip/ScriptToolTip.tsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query';
 import ScriptDetailModal from '../../templates/modals/ScriptDetailModal/ScriptDetailModal';
 import { getLectureByScheduleElement } from '../../../apis/schedule';
 import ScriptIcon from '../../../assets/images/nav/my-script-inactive.svg?react';
+import TrashCan from '../../../assets/images/orange-trash-can.svg?react';
 import styles from './ScriptToolTip.module.scss';
 
 interface IProps {
@@ -25,26 +26,33 @@ const ScriptToolTip = ({ id }: IProps) => {
     setSelectedScriptId(null);
   };
 
-  if (data != null)
-    return (
-      <div className={styles.container}>
-        {data?.map((lecture) => (
-          <div
-            className={styles.tooltipItem}
-            onClick={() => handleToolTipClick(lecture.id)}
-          >
-            {lecture.name}
-            <ScriptIcon />
-          </div>
-        ))}
-        {selectedScriptId !== null && (
-          <ScriptDetailModal
-            scriptId={selectedScriptId}
-            closeModal={handleCloseModal}
-          />
-        )}
+  const handleDeleteBtnClick = () => {};
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.deleteBtnWrapper}>
+        <button className={styles.deleteBtn} onClick={handleDeleteBtnClick}>
+          이 수업 삭제하기
+          <TrashCan />
+        </button>
       </div>
-    );
+      {data?.map((lecture) => (
+        <div
+          className={styles.tooltipItem}
+          onClick={() => handleToolTipClick(lecture.id)}
+        >
+          {lecture.name}
+          <ScriptIcon />
+        </div>
+      ))}
+      {selectedScriptId !== null && (
+        <ScriptDetailModal
+          scriptId={selectedScriptId}
+          closeModal={handleCloseModal}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ScriptToolTip;
