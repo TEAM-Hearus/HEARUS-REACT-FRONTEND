da68b80c2997714b0fb6e8db0c113e9e79dce563
김나연 <apinksky00@gmail.com>
2024-08-10 17:12:19 +0900

    refactor: 일관성을 위해 wai-aria 요소 제거

--- src/components/molecules/RecordTagDropDown/RecordTagDropDown.tsx
+++ src/components/molecules/RecordTagDropDown/RecordTagDropDown.tsx
@@ -38,20 +38,17 @@ const RecordTagDropDown = () => {
       <button
         className={`${styles.tagBtn} ${data !== undefined && data.length > 0 && styles.active}`}
         onClick={handleTagBtnClick}
-        aria-haspopup="listbox"
-        aria-expanded={isTagBtnClicked}
       >
         {tag !== '' ? tag : '태그'}
       </button>
       {isTagBtnClicked && (
-        <ul className={styles.tagsUl} role="listbox">
+        <ul className={styles.tagsUl}>
           {TAGS.map((name) => (
             <li
               key={name}
               className={styles.tagLi}
               onClick={() => handleLiClick(name)}
               role="option"
-              aria-selected={tag === name}
             >
               {name}
             </li>
