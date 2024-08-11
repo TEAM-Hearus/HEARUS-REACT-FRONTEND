4a87751ce481e7892d9b12775f877de6b8dd00c6
김나연 <apinksky00@gmail.com>
2024-08-11 17:40:34 +0900

    design: 소개 페이지

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,6 +1,8 @@
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/logo/landing-logo.svg?react';
-import example from '../../assets/images/landing-myscript.jpg';
+import graph1 from '../../assets/images/landing/graph1.png';
+import graph2 from '../../assets/images/landing/graph2.png';
+import example from '../../assets/images/landing/landing-myscript.jpg';
 import styles from './Landing.module.scss';
 import { SCROLLING_TEXTS } from '../../constants/landing';
 
@@ -29,25 +31,55 @@ const Landing = () => {
       </section>
       <section className={styles.introductionContainer}>
         <article className={styles.upIntro}>
-          <Logo />
-          <p className={styles.hearus}>히어러스</p>
-          <h2>
-            (dummy)인사말, 히어러스는 어떤 가치를 추구하는 서비스인지, 어떻게
-            시작되었는지,
-          </h2>
-          <h2>누구를 대상으로 하는지 설명</h2>
+          <div className={styles.h2Container}>
+            <h2 className={styles.about}>ABOUT HEARUS</h2>
+            <h2 className={styles.aboutKorean}>
+              히어러스는 어떤 서비스인가요?
+            </h2>
+          </div>
+          <div className={styles.h3Container}>
+            <h3 className={styles.h3}>
+              히어러스는 대학교 교내 청각장애 학우 대필지원 도우미 활동에서 느낀
+              문제들을 풀어내기 위해 시작되었습니다.
+            </h3>
+            <h3 className={styles.h3}>
+              청각장애 학우들이 더 나은 환경에서 학습하고, 비장애 학우들과의
+              교육적 불평등을 해소할 수 있도록 하기 위해
+            </h3>
+            <h3 className={styles.h3}>
+              인공지능을 활용한 실시간 음성 텍스트 변환과 문제 생성, 하이라이팅
+              기능을 지닌 서비스입니다.
+            </h3>
+          </div>
         </article>
         <article className={styles.downIntro}>
-          <p>
-            배경 통계 (장애학우의 증가, 도우미 서포터즈의 한계 간단하게 도표
-            등으로 표시)
-          </p>
+          <section className={styles.backGroundIntro}>
+            <p className={styles.title}>Background</p>
+            <span>
+              <p>등록 청각 장애인의 지속적인 성장세 속에서,</p>
+              <p>청각 장애인의 고등 교육기관 진학의 어려움 등의</p>
+              <p>문제들은 오랜 기간동안 해결되지 못했으며</p>
+              <p>수어가 없는 고등 교육 환경에서 청각 장애 학우들은</p>
+              <p>이해하지 못하는 수업을 따라가기 급급했습니다.</p>
+            </span>
+          </section>
+          <section className={styles.graphContainer}>
+            <p>전체 청각장애 등록 장애인 현황</p>
+            <img src={graph1} alt="전체 청각장애 등록 장애인 현황" />
+          </section>
+          <section className={styles.graphContainer}>
+            <p>수어 없는 수업의 내용 이해도 설문조사(2020)</p>
+            <img
+              src={graph2}
+              alt="수어 없는 수업의 내용 이해도 설문조사(2020)"
+            />
+          </section>
         </article>
       </section>
       <section className={styles.functionsContainer}>
         <article className={styles.function}>
           <div className={styles.functionImg}></div>
-          <h3>청각장애학우 지원</h3>
+          <h4>청각장애학우 지원</h4>
           <span className={styles.descriptionContainer}>
             <p>강의 중, 조별과제, 회의 상황에서</p>
             <p>실시간 음성인식 자막을 통해</p>
