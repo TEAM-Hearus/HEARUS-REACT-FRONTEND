c1e41e8e94fed534a52deb7ab68162664463e32f
김나연 <apinksky00@gmail.com>
2024-08-12 22:00:27 +0900

    fix: ul태그로 변경

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -7,12 +7,12 @@ import function2 from '../../assets/images/landing/function2.png';
 import function3 from '../../assets/images/landing/function3.png';
 import example from '../../assets/images/landing/landing-myscript.jpg';
 import styles from './Landing.module.scss';
-import { DOUBLED_TEXTS, SCROLLING_TEXTS } from '../../constants/landing';
+import { SCROLLING_TEXTS } from '../../constants/landing';
 import { useEffect, useRef, useState } from 'react';
 
 const Landing = () => {
   const [activeIndex, setActiveIndex] = useState(1);
-  const containerRef = useRef(null);
+  const carouselRef = useRef<HTMLUListElement>(null);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -143,23 +143,18 @@ const Landing = () => {
       <section className={styles.highLight}>
         {/* 스크롤 애니메이션 구현 예정 */}
         <article className={styles.scrollingContainer}>
-          <div className={styles.scrollingText} ref={containerRef}>
+          <ul className={styles.scrollingText} ref={carouselRef}>
             {SCROLLING_TEXTS.map((text, index) => (
-              <h4
+              <li
                 key={index}
                 className={
-                  index % SCROLLING_TEXTS.length === activeIndex
-                    ? styles.active
-                    : styles.inactive
+                  index === activeIndex ? styles.active : styles.inactive
                 }
-                style={{
-                  transform: `translateY(-${activeIndex * 50}px)`,
-                }}
               >
                 {text}
-              </h4>
+              </li>
             ))}
-          </div>
+          </ul>
         </article>
         <img
           className={styles.exampleImg}
