757241f97b7ccd32480960009f61d368570abc18
Bella <kbee1202@gmail.com>
2024-08-12 18:37:47 +0900

    refactor: 미사용함수 삭제 및 btn -> link로 변경 css수정

--- src/pages/Error/ErrorPage.tsx
+++ src/pages/Error/ErrorPage.tsx
@@ -2,8 +2,8 @@ import styles from './ErrorPage.module.scss';
 import {
   useRouteError,
   isRouteErrorResponse,
-  useNavigate,
   useLocation,
+  Link,
 } from 'react-router-dom';
 import NotFoundImage from '../../assets/images/404image.png';
 
@@ -13,24 +13,16 @@ interface LocationState {
 
 const ErrorPage = () => {
   const error = useRouteError();
-  const navigate = useNavigate();
   const location = useLocation();
   const { errorType } = (location.state as LocationState) || {};
 
   let errorMessage: string = '페이지를 찾을 수 없습니다.';
-  let statusCode: string = '404';
 
   if (errorType === 'auth') {
     errorMessage = '인증 정보가 없습니다.';
-    statusCode = '401';
   } else if (isRouteErrorResponse(error)) {
-    statusCode = error.status.toString();
     errorMessage = error.statusText || errorMessage;
   }
-
-  const goHome = () => {
-    navigate('/home');
-  };
 
   return (
     <div className={styles.errorPageWrapper}>
@@ -38,12 +30,16 @@ const ErrorPage = () => {
         <div className={styles.errorpageBox}>{errorMessage}</div>
       ) : (
         <div className={styles.errorpageBox}>
-          <img src={NotFoundImage} alt="Not found Image" />
-          <h1> {errorMessage}</h1>
-          <h5> 다시 한 번 시도해 주세요</h5>
-          <button className={styles.goHomeBtn} onClick={goHome}>
+          <img
+            className={styles.errorImg}
+            src={NotFoundImage}
+            alt="Not found Image"
+          />
+          <h1 className={styles.errorMsg}> {errorMessage}</h1>
+          <h5 className={styles.errorContent}> 다시 한 번 시도해 주세요</h5>
+          <Link to="/home" className={styles.goHomeBtn}>
             홈으로 돌아가기
-          </button>
+          </Link>
         </div>
       )}
     </div>
