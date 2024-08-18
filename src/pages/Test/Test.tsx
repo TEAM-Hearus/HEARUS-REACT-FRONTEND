1c4918ed14543fbbef7ede364b5eb0f3557236df
김나연 <apinksky00@gmail.com>
2024-08-18 19:50:29 +0900

    refactor: 변수들 순서 정리

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -13,8 +13,13 @@ import styles from './Test.module.scss';
 
 const Test = () => {
   const navigate = useNavigate();
+  const [userAnswers, setUserAnswers] = useState<(string | number)[]>([]);
+  const [showResults, setShowResults] = useState(false);
+
+  const { updateTestData, clearTestData } = useTestModalStore();
   const { lectureId, scheduleElementId, questionCount, questionTypes } =
     useTestSettingsStore();
+
   const inputData = {
     lectureId: lectureId,
     subject: scheduleElementId,
@@ -27,18 +32,6 @@ const Test = () => {
     queryFn: () => generateProblem(inputData),
     retry: false,
   });
-
-  const [userAnswers, setUserAnswers] = useState<(string | number)[]>([]);
-
-  useEffect(() => {
-    if (data != null && data.object != null) {
-      setUserAnswers(Array(data.object.length).fill(''));
-    }
-  }, [data]);
-
-  const [showResults, setShowResults] = useState(false);
-
-  const { updateTestData, clearTestData } = useTestModalStore();
 
   const handleAnswerChange = (index: number, answer: string | number) => {
     const newAnswers = [...userAnswers];
@@ -53,17 +46,23 @@ const Test = () => {
   };
 
   useEffect(() => {
-    if (data != null && data.object != null) {
-      updateTestData({ totalNum: data.object.length });
-    }
-    return () => clearTestData();
-  }, [data]);
-
-  useEffect(() => {
     if (!data?.success) {
       alert('문제 생성을 실패했습니다. 다시 시도해주세요.');
       navigate('/home/test-make');
     }
+  }, [data]);
+
+  useEffect(() => {
+    if (data != null && data.object != null) {
+      setUserAnswers(Array(data.object.length).fill(''));
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (data != null && data.object != null) {
+      updateTestData({ totalNum: data.object.length });
+    }
+    return () => clearTestData();
   }, [data]);
 
   return (
@@ -107,7 +106,6 @@ const Test = () => {
             </section>
           ))}
       </article>
-      {}
     </div>
   );
 };
