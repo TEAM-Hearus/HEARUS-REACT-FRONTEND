e55192c15275d71982552d2108797b3f3b915e12
김나연 <apinksky00@gmail.com>
2024-08-28 17:43:23 +0900

    feat: 사진 가로 스크롤

--- src/components/molecules/landings/ImageCarousel/ImageCarousel.tsx
+++ src/components/molecules/landings/ImageCarousel/ImageCarousel.tsx
@@ -1,33 +1,50 @@
+import { useEffect, useState } from 'react';
 import myScript from '../../../../assets/images/landing/landing-myscript.png';
 import caption from '../../../../assets/images/landing/landing-caption.png';
 import detail from '../../../../assets/images/landing/landing-detail.png';
 import styles from './ImageCarousel.module.scss';
 
+const images = [detail, myScript, caption, detail, myScript];
+const FIRST_IMAGE_INDEX = 1;
+const LAST_IMAGE_INDEX = images.length - 2;
+
 const ImageCarousel = () => {
+  const [activeIndex, setActiveIndex] = useState(FIRST_IMAGE_INDEX);
+  const [transition, setTransition] = useState(true);
+
+  const next = () => {
+    setTransition(true);
+    setActiveIndex((prev) => prev + 1);
+
+    const transitionEndTimer = setTimeout(() => {
+      if (activeIndex === LAST_IMAGE_INDEX) {
+        setTransition(false);
+        setActiveIndex(FIRST_IMAGE_INDEX);
+      }
+    }, 2000);
+
+    return () => clearTimeout(transitionEndTimer);
+  };
+
+  useEffect(() => {
+    const slideInterval = setInterval(next, 3000);
+    return () => clearInterval(slideInterval);
+  }, [activeIndex]);
+
   return (
     <article className={styles.container}>
-      <ul className={styles.ul}>
-        <li className={styles.li}>
-          <img
-            className={styles.img}
-            src={myScript}
-            alt="recent script page example"
-          />
-        </li>
-        <li className={styles.li}>
-          <img
-            className={styles.img}
-            src={caption}
-            alt="record caption page example"
-          />
-        </li>
-        <li className={styles.li}>
-          <img
-            className={styles.img}
-            src={detail}
-            alt="script detail page example"
-          />
-        </li>
+      <ul
+        className={styles.ul}
+        style={{
+          transform: `translateX(calc(-${activeIndex - 1} * (50dvw + 50px)))`,
+          transition: transition ? 'transform 2s ease' : 'none',
+        }}
+      >
+        {images.map((img, index) => (
+          <li key={index} className={styles.li}>
+            <img className={styles.img} src={img} alt={`Image ${index + 1}`} />
+          </li>
+        ))}
       </ul>
     </article>
   );
