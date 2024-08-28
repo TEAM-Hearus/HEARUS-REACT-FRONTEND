86565a2c834dc2cb406c7dc4f253d6375979991e
김나연 <apinksky00@gmail.com>
2024-08-28 15:08:18 +0900

    refactor: 텍스트 캐러셀 컴포넌트 분리

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
 import { Link, Navigate } from 'react-router-dom';
+import TextCarousel from '../../components/molecules/landings/TextCarousel/TextCarousel';
 import { checkAuthentication } from '../../utils/auth';
 import Logo from '../../assets/images/logo/landing-logo.svg?react';
 import graph1 from '../../assets/images/landing/graph1.png';
@@ -8,35 +8,9 @@ import function1 from '../../assets/images/landing/function1.png';
 import function2 from '../../assets/images/landing/function2.png';
 import function3 from '../../assets/images/landing/function3.png';
 import example from '../../assets/images/landing/landing-myscript.jpg';
-import { SCROLLING_TEXTS } from '../../constants/landing';
 import styles from './Landing.module.scss';
 
 const Landing = () => {
-  const FIRST_TEXT_INDEX = 2;
-  const LAST_TEXT_INDEX = SCROLLING_TEXTS.length - 3;
-
-  const [activeIndex, setActiveIndex] = useState(FIRST_TEXT_INDEX);
-  const [transition, setTransition] = useState(true);
-
-  const next = () => {
-    setTransition(true);
-    setActiveIndex((prev) => prev + 1);
-
-    const transitionEndTimer = setTimeout(() => {
-      if (activeIndex === LAST_TEXT_INDEX) {
-        setTransition(false);
-        setActiveIndex(FIRST_TEXT_INDEX);
-      }
-    }, 300);
-
-    return () => clearTimeout(transitionEndTimer);
-  };
-
-  useEffect(() => {
-    const slideInterval = setInterval(next, 3000);
-    return () => clearInterval(slideInterval);
-  }, [next]);
-
   const isAuthenticated = checkAuthentication();
 
   if (isAuthenticated) {
@@ -161,26 +135,7 @@ const Landing = () => {
         </div>
       </section>
       <section className={styles.highLight}>
-        <article className={styles.scrollingContainer}>
-          <ul
-            className={styles.scrollingText}
-            style={{
-              transform: `translateY(-${(activeIndex - 1) * 55}px)`,
-              transition: transition ? 'transform 0.3s ease' : 'none',
-            }}
-          >
-            {SCROLLING_TEXTS.map((text, index) => (
-              <li
-                key={index}
-                className={
-                  index === activeIndex ? styles.active : styles.inactive
-                }
-              >
-                {text}
-              </li>
-            ))}
-          </ul>
-        </article>
+        <TextCarousel />
         <img
           className={styles.exampleImg}
           src={example}
