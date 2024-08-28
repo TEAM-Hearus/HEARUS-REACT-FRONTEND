1b929d34a470c726cc16824d6875f4c6b0c52170
김나연 <apinksky00@gmail.com>
2024-08-28 17:23:22 +0900

    design: 캐러셀 사진들 ul로 넣기

--- src/components/molecules/landings/ImageCarousel/ImageCarousel.tsx
+++ src/components/molecules/landings/ImageCarousel/ImageCarousel.tsx
@@ -1,14 +1,34 @@
-import example from '../../../../assets/images/landing/landing-myscript.jpg';
+import myScript from '../../../../assets/images/landing/landing-myscript.png';
+import caption from '../../../../assets/images/landing/landing-caption.png';
+import detail from '../../../../assets/images/landing/landing-detail.png';
 import styles from './ImageCarousel.module.scss';
 
 const ImageCarousel = () => {
   return (
-    <article>
-      <img
-        className={styles.exampleImg}
-        src={example}
-        alt="recent script page example"
-      />
+    <article className={styles.container}>
+      <ul className={styles.ul}>
+        <li className={styles.li}>
+          <img
+            className={styles.img}
+            src={myScript}
+            alt="recent script page example"
+          />
+        </li>
+        <li className={styles.li}>
+          <img
+            className={styles.img}
+            src={caption}
+            alt="record caption page example"
+          />
+        </li>
+        <li className={styles.li}>
+          <img
+            className={styles.img}
+            src={detail}
+            alt="script detail page example"
+          />
+        </li>
+      </ul>
     </article>
   );
 };
