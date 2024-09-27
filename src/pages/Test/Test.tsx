8019b48fde587fd418eb7121f55c5a990dd56cb3
김나연 <apinksky00@gmail.com>
2024-09-27 23:55:09 +0900

    refactor: useEffect 최적화

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -50,17 +50,9 @@ const Test = () => {
       alert('문제 생성을 실패했습니다. 다시 시도해주세요.');
       navigate('/home/test-make');
     }
-  }, [data]);
-
-  useEffect(() => {
-    if (data != null && data.object != null) {
-      setUserAnswers(Array(data.object.length).fill(''));
-    }
-  }, [data]);
-
-  useEffect(() => {
     if (data != null && data.object != null) {
       updateTestData({ totalNum: data.object.length });
+      setUserAnswers(Array(data.object.length).fill(''));
     }
     return () => clearTestData();
   }, [data]);
